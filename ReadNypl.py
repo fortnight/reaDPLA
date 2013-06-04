bb1e51ef04e4bf7977ad19994d01d00ea3418b84
fortnight <fernando.ellis@gmail.com>
2013-06-04 19:22:43 -0400

    Update ReadNypl.py

--- ReadNypl.py
+++ ReadNypl.py
@@ -1,1 +1,36 @@
-woop
+import argparse
+
+def read_nypl():
+    f = open('nypl', 'r')
+    g = open('copy', 'r+')
+    i = 0;
+    temp_str = ""
+    for line in f:
+        temp_str = line
+        g.write(temp_str)
+        i += 1
+        print i
+    print "The deed is done"
+
+def print_titles():
+    f = open('copy', 'r')
+    g = open('titles', 'w')
+    i = 0;
+    titles = ""
+    while f.read(1) != "":
+        f.seek(i)
+        if f.read(5) == 'title':
+            #print "wooop"
+            j = i+6
+            while (f.read(1)!= ',')and(f.read(4)!='nfo"')and(f.read(10)!=':"Lawrence')and(f.read(10)!=" 'Laurie a"):
+                f.seek(j)
+                titles += f.read(1)
+                j += 1
+            titles += '\n'
+        i += 1;
+    g.write(titles)
+
+
+if __name__ == "__main__":
+    read_nypl()
+    print_titles()
